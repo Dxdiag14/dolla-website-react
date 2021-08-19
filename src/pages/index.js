cf43d713306d2d084760ddad4c4957f52863dc7c
HJ Miranda <hjmiranda02@gmail.com>
2021-08-19 13:21:56 +0800

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Navbar from '../components/Navbar/index'
+import Sidebar from '../components/Sidebar'
+import HeroSection from '../components/HeroSection'
+import {useState} from 'react'
+import InfoSection from '../components/InfoSection'
+import { homeObjOne, homeObjTwo, homeObjThree } from '../components/InfoSection/Data'
+import Services from '../components/Services'
+import Footer from '../components/Footer'
+
+const Home = () => {
+    const [isOpen, SetIsOpen] = useState(false)
+
+    const toggle = () => {
+        SetIsOpen(!isOpen)
+    }
+
+    return (
+        <>
+            <Sidebar isOpen={isOpen} toggle={toggle}/>
+            <Navbar toggle={toggle}/>
+            <HeroSection />
+            <InfoSection {...homeObjOne} />
+            <InfoSection {...homeObjTwo} />
+            <Services />
+            <InfoSection {...homeObjThree} />
+            <Footer />
+        </>
+    )
+}
+
+export default Home
